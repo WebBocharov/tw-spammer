f5d42527f8399ccf59b80fb119940922303199fe
web.bocharov <web.bocharov@gmail.com>
2024-11-14 17:40:51 +0200

    Refactor API endpoint calls and add request backoff.
    Simplified API endpoint URL usage by consolidating into class variables. Implemented backoff mechanism to handle handling.

--- spammer/selectors.py
+++ spammer/selectors.py
@@ -1,5 +1,4 @@
 class Selectors:
     TEXT_FIELD = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/section[2]/div/div/div[2]/div/div/aside/div[2]/div[2]/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div'
-    FILE_INPUT = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/section[2]/div/div/div[2]/div/div/aside/div[2]/div[1]/input'
-    FILE_CHOOSER = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/section[2]/div/div/div/2/div/div/aside/div[2]/div[2]/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/input'
-    SUBMIT_BUTTON = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/section[2]/div/div/div[2]/div/div/aside/div[2]/button'
+    FILE_INPUT = '//input[contains(@accept, "image/jpeg,image/png,image/webp,image/gif")][@type="file"][@data-testid="fileInput"]'
+    SUBMIT_BUTTON = '//button[@role="button"][@type="button"][@data-testid="dmComposerSendButton"]'
