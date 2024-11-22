7c7fa120c2018b50d0f38203c7cdea43a2f25042
web.bocharov <web.bocharov@gmail.com>
2024-11-22 17:33:56 +0200

    Remove unused update_browser_list functionality
    Deleted the unused `update_browser_list.py` file and its imports. Enhanced the `update_groups_urls.py` to fetch and store Twitter conversation URLs asynchronously. This also included refactoring to separate parsing logic for better maintainability.

--- app/events/update_browser_list.py
+++
@@ -1,4 +0,0 @@
-async def update_browser_list():
-    pass
-    # await browsers_list_view.set_controls(await BrowserProfileController.get_browser_profiles())
-    # return page
--- app/events/update_groups_urls.py
+++ app/events/update_groups_urls.py
@@ -1,16 +1,47 @@
+import asyncio
+
 import flet as ft
+from bs4 import BeautifulSoup
 from loguru import logger
 
 from apis import ADSPowerLocalAPI
-from database.controllers import BrowserProfileController
-from spammer.spammer import TwitterSpammer
+from database.controllers import BrowserProfileController, TwitterGroupUrlController
+from spammer import TwitterSpammer
 
 
 async def update_groups_urls_callback(event: ft.ControlEvent):
-    # browser = await BrowserProfileController.get_browser_profile_by_id('kp73d9m')
     browsers = await BrowserProfileController.get_browser_profiles()
 
     for browser in browsers:
         browser_connection_data = await ADSPowerLocalAPI.open_browser(browser.browser_id)
         logger.info(f"Getting groups urls for browser: {browser.browser_id}")
-        await TwitterSpammer.get_data_for_request(browser_connection_data, browser.browser_id)
+        asyncio.create_task(parse_groups(browser_connection_data, browser.browser_id))
+
+
+async def parse_groups(browser_connection, browser_id):
+    logger.info(f"Getting page source for browser {browser_id}")
+    page_source = await TwitterSpammer.get_page_source(browser_connection)
+    logger.info(f"Got page source for browser {browser_id}")
+
+    bs4 = BeautifulSoup(page_source, 'html.parser')
+    bs4_conv = bs4.find('main', {'role': 'main'}).find('div', {'role': 'tablist'}).find_all(recursive=False)
+    conversation_urls = []
+
+    for conversation in bs4_conv:
+        conv_url = conversation.find_next(
+            'a',
+            {'data-testid': 'DM_Conversation_Avatar', 'role': 'link'}
+        ).get('href')
+        logger.info(f"Getting conversation url {conv_url}")
+
+        if conv_url is not None:
+            conversation_urls.append(f"https://x.com/messages/{conv_url.split('/')[2]}")
+        else:
+            logger.info(f"Conversation url is None - {conv_url}")
+
+    await TwitterGroupUrlController.batch_create_by_browser_id(
+        set(conversation_urls),
+        browser_id
+    )
+
+    logger.info(f"Added {len(set(conversation_urls))} urls to database")
